c7986bb4abddcce73ffd2a06901683c8066ca9fc
YoCheng <xyox860224@gmail.com>
2020-11-07 15:14:47 +0800

    Optimal code.

--- YoPCNet/test.py
+++ YoPCNet/test.py
@@ -8,16 +8,19 @@ import torch.optim as optim
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
+import itertools
 import cv2
 
 from pointnetX.model import PointNetCls, feature_transform_regularizer
 from pointnetX.dataset_hdl32 import NPCDataset
 
-import itertools
-def plot_confusion_matrix(cm, classes,
+
+def plot_confusion_matrix(cm, 
+                          classes,
                           normalize=False,
                           title='Confusion matrix',
-                          cmap=plt.cm.Blues):
+                          cmap="Blues"):
+                          
     """
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
@@ -52,55 +55,80 @@ def plot_confusion_matrix(cm, classes,
 
 
 if __name__ == "__main__":
+    # Parse
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataroot', type=str, default="/home/yoyo/hdl32_data", help='input data root')
     parser.add_argument('--model', type=str, help='epochs')
     parser.add_argument('--feature_transform', action='store_true', help="use feature transform")
+    parser.add_argument('--batchSize', type=int, default=20, help='input batch size')
     args = parser.parse_args()
+    
+    # class_list=["Others", "Person", "Car"]
+    class_list=["Others", "Person", "Car", "Moto"]
 
-    # data
+    # Data
+    print("Prepare Dataset.")
     dataset_test = NPCDataset(args.dataroot, testing=True)
     dataloader_test = torch.utils.data.DataLoader(
         dataset_test,
-        batch_size=20,
+        batch_size=args.batchSize,
         shuffle=False,
         num_workers=4)
-
-    # model load
-    classifier = PointNetCls(k=4, feature_transform=args.feature_transform)
+    
+    # Model load
+    print("Make model.")
+    classifier = PointNetCls(k=len(class_list), feature_transform=args.feature_transform)
     classifier.load_state_dict(torch.load(args.model))
     classifier.cuda()
 
-    CM = np.zeros((4, 4), dtype=np.int)
+    # For statistics 
+    CM = np.zeros((len(class_list), len(class_list)), dtype=np.int)
     tlist = []
     failNum = 0
+    dataNum = len(dataset_test)
+
+    # Test start
     for data in dataloader_test:
+        # Time start
         t = time.time()
+        
+        # Get Data
         pc, label, img, artF, voxel = data
         label = label[:, 0]
         pc = pc.transpose(2, 1)
         pc, label, img, artF, voxel = pc.cuda(), label.cuda(), img.cuda(), artF.cuda(), voxel.cuda()
+
+        # Eval model
         classifier = classifier.eval()
         pred, trans, trans_feat = classifier(pc, img, artF, voxel)
-        loss = torch.nn.functional.nll_loss(pred, label)
+
+        loss = torch.nn.functional.cross_entropy(pred, label)
+        # loss = torch.nn.functional.nll_loss(pred, label)
         if args.feature_transform:
             loss += feature_transform_regularizer(trans_feat) * 0.001
 
+        # Predict Result
         pred_choice = pred.data.max(1)[1]
-        
         pred_List = pred_choice.data.cpu().numpy()
         label_List = label.data.cpu().numpy()
-        tlist.append(time.time()-t)
-        CM += np.asarray(confusion_matrix(label_List, pred_List, labels=[0, 1, 2, 3]), dtype=np.int)
 
+        # Time stop
+        tlist.append(time.time()-t)
+
+        # Make confusion_matrix
+        CM += np.asarray(confusion_matrix(label_List, pred_List, labels=list(range(len(class_list)))), dtype=np.int)
+
+        # Check Fail Img
         failList = (pred_List != label_List)
         for i in range(len(failList)):
             if failList[i]==True:
                 failImg = img[i].cpu().numpy()
-                cv2.imwrite("{0}__({1}, {2}).png".format(failNum, pred_List[i], label_List[i]), failImg)
+                os.makedirs("./failImg", exist_ok=True)
+                cv2.imwrite("./failImg/{0}__(p{1}, g{2}).png".format(failNum, pred_List[i], label_List[i]), failImg)
                 failNum+=1
 
-    print("Spent time per 20 objs.(out first): ", sum(tlist[1:]) / 280.0 * 20.0)
-    print("Accu: ", (CM[0, 0]+CM[1, 1]+CM[2, 2]+CM[3, 3])/200.0)
+    # Result
+    print("Spent time per {0} objs.(out first): {1}".format(args.batchSize, (sum(tlist[1:]) / dataNum) * args.batchSize))
+    print("Accu: ", sum([CM[i, i] for i in range(len(class_list))]) / dataNum)
     plot_confusion_matrix(CM, classes=["Others", "Person", "Car", "Moto"], title="Classification")
     plt.show()
--- YoPCNet/train.py
+++ YoPCNet/train.py
@@ -10,11 +10,13 @@ import matplotlib.pyplot as plt
 from pointnetX.model import PointNetCls, feature_transform_regularizer
 from pointnetX.dataset_hdl32 import NPCDataset
 
+
 blue = lambda x: '\033[94m' + x + '\033[0m'
-keepTrAccu = []
-keepTeAccu = []
-keepTeloss = []
-keepTrloss = []
+keepAccu_train = []
+keeploss_train = []
+keepAccu_test = []
+keeploss_test = []
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -26,6 +28,10 @@ if __name__ == "__main__":
     parser.add_argument('--feature_transform', action='store_true', help="use feature transform")
     args = parser.parse_args()
 
+    # class_list=["Others", "Person", "Car"]
+    class_list=["Others", "Person", "Car", "Moto"]
+
+    # Data
     print("Prepare Dataset.")
     dataset = NPCDataset(args.dataroot)
     dataloader = torch.utils.data.DataLoader(
@@ -37,18 +43,20 @@ if __name__ == "__main__":
     dataset_test = NPCDataset(args.dataroot, testing=True)
     dataloader_test = torch.utils.data.DataLoader(
         dataset_test,
-        batch_size=25,
+        batch_size=args.batchSize,
         shuffle=True,
         num_workers=4)
 
+    # Make Dir
     try:
         print("mkdir ", args.outf)
         os.makedirs(args.outf)
     except OSError:
         pass
 
+    # Model load
     print("Make model.")
-    classifier = PointNetCls(k=4, feature_transform=args.feature_transform)
+    classifier = PointNetCls(k=len(class_list), feature_transform=args.feature_transform)
     if args.model:
         classifier.load_state_dict(torch.load(args.model), feature_transform=args.feature_transform)
 
@@ -56,63 +64,96 @@ if __name__ == "__main__":
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
     classifier.cuda()
 
+    # Train
     print("Training start.")
-    # train
     num_batch = len(dataset) // args.batchSize
     for epoch in range(1, args.epochs + 1):
+        # Train
+        loss_sum = 0
+        corr_sum = 0
+        dataNum = float(len(dataset))
+
         for i, data in enumerate(dataloader, 0):
+            # Get Data
             pc, label, img, artF, voxel = data
             tbSize = len(label)
             label = label[:, 0]
             pc = pc.transpose(2, 1)
             pc, label, img, artF, voxel = pc.cuda(), label.cuda(), img.cuda(), artF.cuda(), voxel.cuda()
+            
+            # Train
             optimizer.zero_grad()
             classifier = classifier.train()
             pred, trans, trans_feat = classifier(pc, img, artF, voxel)
+
             loss = torch.nn.functional.cross_entropy(pred, label)
             # loss = torch.nn.functional.nll_loss(pred, label)
             if args.feature_transform:
                 loss += feature_transform_regularizer(trans_feat) * 0.001
+            
             loss.backward()
             optimizer.step()
+
+            # Predict Result
             pred_choice = pred.data.max(1)[1]
             correct = pred_choice.eq(label.data).cpu().sum()
+            loss_sum += loss.item()
+            corr_sum += correct.item()
             print('[%5d: %3d/%3d] train loss: %f accuracy: %f' % (epoch, i, num_batch, loss.item(), correct.item() / float(tbSize)), end='\r')
 
-        keepTrAccu += [correct.item() / float(tbSize)]
-        keepTrloss += [loss.item()]
+        loss = loss_sum / dataNum
+        accu = corr_sum / dataNum
+        print('[%5d: %3d/%3d] train loss: %f accuracy: %f' % (epoch, i, num_batch, loss, accu))
+        keepAccu_train += [accu]
+        keeploss_train += [loss]
         
         # Test
-        t, data = next(enumerate(dataloader_test, 0))
-        pc, label, img, artF, voxel = data
-        tbSize = len(label)
-        label = label[:, 0]
-        pc = pc.transpose(2, 1)
-        pc, label, img, artF, voxel = pc.cuda(), label.cuda(), img.cuda(), artF.cuda(), voxel.cuda()
-        classifier = classifier.eval()
-        pred, trans, trans_feat = classifier(pc, img, artF, voxel)
-        loss = torch.nn.functional.nll_loss(pred, label)
-        if args.feature_transform:
-            loss += feature_transform_regularizer(trans_feat) * 0.001
-        pred_choice = pred.data.max(1)[1]
-        correct = pred_choice.eq(label.data).cpu().sum()
-        print('[%5d:-------->  %s loss: %f accuracy: %f' % (epoch, blue('test'), loss.item(), correct.item() / float(tbSize)))
+        loss_sum = 0
+        corr_sum = 0
+        dataNum = len(dataset_test)
+        for i, data in enumerate(dataloader_test, 0):
+            # Get Data
+            pc, label, img, artF, voxel = data
+            tbSize = len(label)
+            label = label[:, 0]
+            pc = pc.transpose(2, 1)
+            pc, label, img, artF, voxel = pc.cuda(), label.cuda(), img.cuda(), artF.cuda(), voxel.cuda()
 
-        keepTeAccu += [correct.item() / float(tbSize)]
-        keepTeloss += [loss.item()]
+            # Eval model
+            classifier = classifier.eval()
+            pred, trans, trans_feat = classifier(pc, img, artF, voxel)
+
+            loss = torch.nn.functional.cross_entropy(pred, label)
+            # loss = torch.nn.functional.nll_loss(pred, label)
+            if args.feature_transform:
+                loss += feature_transform_regularizer(trans_feat) * 0.001
+            
+            # Predict Result
+            pred_choice = pred.data.max(1)[1]
+            correct = pred_choice.eq(label.data).cpu().sum()
+            loss_sum += loss.item()
+            corr_sum += correct.item()
+
+        loss = loss_sum / dataNum
+        accu = corr_sum / dataNum
+        print(blue(' %5d:-------->  test loss: %f accuracy: %f' % (epoch, loss, accu)))
+        keepAccu_test += [accu]
+        keeploss_test += [loss]
         
-        if epoch > 5:
-            if keepTeAccu[epoch-1] >= max(keepTeAccu) or (epoch > 4500 and epoch % 100 == 0):
-                torch.save(classifier.state_dict(), '%s/fcePI_cls_model_%d_%f.pth' % (args.outf, epoch, keepTeAccu[epoch-1]))
         scheduler.step()
 
-        # vis
+        # Save Model
+        if epoch > 5:
+            if keepAccu_test[-1] >= max(keepAccu_test) or (epoch > 4500 and epoch % 100 == 0):
+                torch.save(classifier.state_dict(), '%s/fcePI_cls_model_%d_%f.pth' % (args.outf, epoch, keepAccu_test[-1]))
+
+        # Visualize
         plt.ylim(0, 1)
-        plt.plot(keepTrAccu, "b")
-        plt.plot(keepTeAccu, "r")
-        plt.savefig("./F1.png")
+        plt.plot(keepAccu_train, "b")
+        plt.plot(keepAccu_test, "r")
+        plt.savefig("./Accu.png")
         plt.cla()
-        plt.plot(keepTrloss, "b")
-        plt.plot(keepTeloss, "r")
-        plt.savefig("./F2.png")
+        plt.plot(keeploss_train, "b")
+        plt.plot(keeploss_test, "r")
+        plt.savefig("./Loss.png")
         plt.cla()
